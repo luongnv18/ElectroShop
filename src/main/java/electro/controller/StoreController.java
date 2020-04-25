aa8a73502048c3bd8f77358e7fc4cac903fdf596
nquangtoan98 <nquangtoan98@gmail.com>
2020-04-25 17:36:15 +0700

    them store

---
+++ src/main/java/electro/controller/StoreController.java
@@ -0,0 +1,37 @@
+package electro.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+import electro.entity.DanhMuc;
+import electro.entity.SanPham;
+import electro.service.DanhMucSPService;
+import electro.service.SanPhamService;
+
+@Controller
+@RequestMapping("/store")
+public class StoreController {
+	
+	@Autowired
+	DanhMucSPService danhMucSPService;
+	
+	@Autowired
+	SanPhamService sanPhamService;
+	
+	@GetMapping
+	public String Default(ModelMap modelMap) {
+		List<DanhMuc> lstDanhMucs= danhMucSPService.GetListDanhMuc();
+		List<SanPham> lstSanPhams = sanPhamService.GetListSanPham();
+		modelMap.addAttribute("lstDanhMuc",lstDanhMucs);
+		modelMap.addAttribute("lstSP", lstSanPhams);
+		return "store";
+	}
+}
+	
+	
