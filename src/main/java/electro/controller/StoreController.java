055e11b5b269a85b5b42f1bb1f3d6a49ae53509b
Luong <tobe4961@gmail.com>
2020-05-02 11:07:02 +0700

    sua session email thanh user

--- src/main/java/electro/controller/StoreController.java
+++ src/main/java/electro/controller/StoreController.java
@@ -8,6 +8,7 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 import electro.entity.DanhMuc;
 import electro.entity.SanPham;
@@ -17,7 +18,7 @@ import electro.service.SanPhamService;
 import electro.service.ThuongHieuSPService;
 
 @Controller
-@SessionAttributes({ "email", "giohang" })
+@SessionAttributes({ "user", "giohang" })
 @RequestMapping("/SanPham")
 public class StoreController {
 
